2260d9a99618287e82639bb24f5a10347725a265
Anton <razjivin.anton@yandex.ru>
2023-07-23 23:16:32 +0300

    add example_67

---
+++ example_67/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 67: Yапишите программу, которая принимать на вход число
+и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int SumDigits(int num) {
+    if( num == 0) return 0;
+    return num % 10 + SumDigits(num / 10);
+}
+
+Console.WriteLine(SumDigits(45));
+
+// int FactorialRec(int n) {
+//     if( n == 1) return 1;
+//     else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(5));
